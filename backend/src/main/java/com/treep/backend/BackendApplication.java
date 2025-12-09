8539f5be2db9469d13e6a531673e79110aaa27b1
Antoine Goossens <antoine.goossens@condorcet.be>
2025-12-09 21:42:18 +0100

    fix: On scanne tous les composants, au lieu de ceux uniquement dans .service

--- backend/src/main/java/com/treep/backend/BackendApplication.java
+++ backend/src/main/java/com/treep/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.treep.backend.service;
+package com.treep.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
