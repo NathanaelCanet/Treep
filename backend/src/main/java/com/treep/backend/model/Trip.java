59ac5840096e9b5cb8dba445a0333d9db53c41e8
AntoineHEPH <antoine.goossens@condorcet.be>
2025-12-16 19:13:11 +0100

    feat(backend): Association entre le user et trip

--- backend/src/main/java/com/treep/backend/model/Trip.java
+++ backend/src/main/java/com/treep/backend/model/Trip.java
@@ -4,11 +4,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -23,6 +28,10 @@ public class Trip {
     private LocalDate dateFin;
     private Double budgetTotal;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
 
     @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Activity> activities = new ArrayList<>();
@@ -79,5 +88,13 @@ public class Trip {
 
     public void setActivities(List<Activity> activities) {
         this.activities = activities;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
